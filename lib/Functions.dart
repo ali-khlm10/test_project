26cf512979394d35390e80b55a656adbff65adfa
Mat <khodaei.alireza76@gmail.com>
2022-08-07 03:30:45 +0430

    modified many file

---
+++ lib/Functions.dart
@@ -0,0 +1,179 @@
+  // return Scaffold(
+    //   body: Stack(children: [
+    // Container(
+    //   decoration: const BoxDecoration(
+    //       image: DecorationImage(
+    //           image: AssetImage("assets/images/image1.png"),
+    //           fit: BoxFit.fill)),
+    //   height: 3 * height / 5,
+    //   width: width,
+    // ),
+    // Container(
+    //   margin: EdgeInsets.only(
+    //       top: height * .33,
+    //       bottom: height * .48,
+    //       left: width / 2,
+    //       right: width * .02),
+    //   decoration: const BoxDecoration(
+    //       borderRadius: BorderRadius.all(Radius.circular(30)),
+    //       color: Colors.red),
+    // ),
+    // Container(
+    //   margin: EdgeInsets.only(
+    //       top: height * .44,
+    //       bottom: height * .37,
+    //       left: width * .81,
+    //       right: width * .02),
+    //   decoration: const BoxDecoration(
+    //       borderRadius: BorderRadius.all(Radius.circular(30)),
+    //       color: Colors.lightBlue),
+    //   child: const Center(
+    //     child: Icon(
+    //       Icons.location_on_outlined,
+    //       size: 40,
+    //     ),
+    //   ),
+    // ),
+    // Container(
+    //   margin: EdgeInsets.only(
+    //       top: height * .44,
+    //       bottom: height * .37,
+    //       left: width * .30,
+    //       right: width * .22),
+    //   decoration: const BoxDecoration(
+    //       borderRadius: BorderRadius.all(Radius.circular(30)),
+    //       color: Colors.green),
+    // ),
+    // Container(
+    //   margin: EdgeInsets.only(top: height * .56),
+    //   decoration: const BoxDecoration(
+    //       borderRadius: BorderRadius.only(
+    //         topRight: Radius.circular(20),
+    //         topLeft: Radius.circular(20),
+    //       ),
+    //       color: Colors.orange),
+    //   child: Column(
+    //     // crossAxisAlignment: CrossAxisAlignment.stretch,
+    //     children: <Widget>[
+    //       Expanded(
+    //         flex: 1,
+    //         child: Container(),
+    //       ),
+    //       Expanded(
+    //         flex: 2,
+    //         child: Container(
+    //           color: Colors.blueAccent,
+    //           child: TextField(
+    //             onChanged: (value) {
+    //               setState(() {});
+    //             },
+    //           ),
+    //         ),
+    //       ),
+    //       Expanded(
+    //         flex: 2,
+    //         child: SizedBox(
+    //           height: 80.0,
+    //           child: ListView(
+    //             scrollDirection: Axis.horizontal,
+    //             children: List.generate(20, (int index) {
+    //               return Return_little_cards(Index: index);
+    //             }),
+    //           ),
+    //         ),
+    //       ),
+    //       Expanded(
+    //         flex: 5,
+    //         child: SizedBox(
+    //           height: 80.0,
+    //           child: ListView(
+    //             scrollDirection: Axis.horizontal,
+    //             children: List.generate(20, (int index) {
+    //               return Return_big_cards(Index: index);
+    //             }),
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // ),
+    // ]),
+    //   bottomNavigationBar: CircleNavBar(
+    //     activeIcons: const [
+    //       Icon(Icons.person),
+    //       Icon(Icons.search),
+    //       Icon(Icons.location_on_outlined),
+    //       Icon(Icons.shopping_cart_outlined),
+    //       Icon(Icons.home_outlined),
+    //     ],
+    //     inactiveIcons: [
+    //       Container(
+    //         padding: const EdgeInsets.only(top: 8),
+    //         child: Column(
+    //           children: const [
+    //             Icon(Icons.person),
+    //             Text('پروفایل'),
+    //           ],
+    //         ),
+    //       ),
+    //       Container(
+    //         padding: const EdgeInsets.only(top: 8),
+    //         child: Column(
+    //           children: const [
+    //             Icon(Icons.search),
+    //             Text('جستجو'),
+    //           ],
+    //         ),
+    //       ),
+    //       Container(
+    //         padding: const EdgeInsets.only(top: 8),
+    //         child: Column(
+    //           children: const [
+    //             Icon(Icons.location_on_outlined),
+    //             Text('لوکیشن'),
+    //           ],
+    //         ),
+    //       ),
+    //       Container(
+    //         padding: const EdgeInsets.only(top: 8),
+    //         child: Column(
+    //           children: const [
+    //             Icon(Icons.shopping_cart_outlined),
+    //             Text('فروشگاه'),
+    //           ],
+    //         ),
+    //       ),
+    //       Container(
+    //         padding: const EdgeInsets.only(top: 8),
+    //         child: Column(
+    //           children: const [
+    //             Icon(Icons.home_outlined),
+    //             Text('خانه'),
+    //           ],
+    //         ),
+    //       ),
+    //     ],
+    //     color: Colors.white,
+    //     height: 60,
+    //     circleWidth: 60,
+    //     initIndex: 1,
+    //     onChanged: (v) {
+    //       // todo
+    //     },
+    //     // tabCurve: ,
+    //     // padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
+    //     // cornerRadius: const BorderRadius.only(
+    //     //   topLeft: Radius.circular(8),
+    //     //   topRight: Radius.circular(8),
+    //     //   bottomRight: Radius.circular(24),
+    //     //   bottomLeft: Radius.circular(24),
+    //     // ),
+    //     shadowColor: Colors.black,
+    //     elevation: 2,
+    //   ),
+    // );
+
+    import 'package:flutter/material.dart';
+
+
+    
